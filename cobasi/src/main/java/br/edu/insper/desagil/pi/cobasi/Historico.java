e07f0015f6201f529be7ad7162cef351a8f4af8b
Stefane R.R. <stefanerr@al.insper.edu.br>
2025-09-16 21:29:45 -0300

    Refatoração

--- cobasi/src/main/java/br/edu/insper/desagil/pi/cobasi/Historico.java
+++ cobasi/src/main/java/br/edu/insper/desagil/pi/cobasi/Historico.java
@@ -2,20 +2,17 @@ package br.edu.insper.desagil.pi.cobasi;
 
 import java.time.LocalDate;
 
+
 public class Historico {
-    private String nomeTutor;
-    private String enderecoTutor;
-    private String telefoneTutor;
+    private Tutor tutor;
     private String nome;
     private int tipo;
     private LocalDate dataNascimento;
     private double peso;
     private String extra;
 
-    public Historico(String nomeTutor, String nome, int tipo, LocalDate dataNascimento, double peso, String extra) {
-        this.nomeTutor = nomeTutor;
-        this.enderecoTutor = null;
-        this.telefoneTutor = null;
+    public Historico(Tutor tutor,String nome, int tipo, LocalDate dataNascimento, double peso, String extra) {
+        this.tutor=tutor;
         this.nome = nome;
         this.tipo = tipo;
         this.dataNascimento = dataNascimento;
@@ -23,44 +20,26 @@ public class Historico {
         this.extra = extra;
     }
 
-    public void setEnderecoTutor(String enderecoTutor) {
-        this.enderecoTutor = enderecoTutor;
-    }
-
-    public void setTelefoneTutor(String telefoneTutor) {
-        this.telefoneTutor = telefoneTutor;
-    }
-
-    public String resumoTutor() {
-        String summary = "Tutor: " + nomeTutor + "\n";
-        if (enderecoTutor != null) {
-            summary += "Endereço: " + enderecoTutor + "\n";
-        }
-        if (telefoneTutor != null) {
-            summary += "Telefone: " + telefoneTutor + "\n";
-        }
-        return summary;
-    }
 
     public String resumo() {
-        String summary = "Animal:" + nome + "\n";
+        String summary = tutor.resumoTutor();
+        summary += "Animal: " + nome + "\n";
+        summary += "Nascimento: " + dataNascimento.toString() + "\n";
+        summary += "Peso: " + peso + "kg\n";
+
         if (tipo == 0) {
-            summary += "Nascimento: " + dataNascimento.toString() + "\n";
-            summary += "Peso: " + peso + "kg\n";
             summary += "Banho: " + extra + "\n";
         } else if (tipo == 1) {
-            summary += "Nascimento: " + dataNascimento.toString() + "\n";
-            summary += "Peso: " + peso + "kg\n";
             summary += "Doença: " + extra + "\n";
         } else if (tipo == 2) {
-            summary += "Nascimento: " + dataNascimento.toString() + "\n";
-            summary += "Peso: " + peso + "kg\n";
             summary += "Venenosa: " + Boolean.parseBoolean(extra) + "\n";
         } else if (tipo == 3) {
-            summary += "Nascimento: " + dataNascimento.toString() + "\n";
-            summary += "Peso: " + peso + "kg\n";
             summary += "Comida: " + Integer.parseInt(extra) + "\n";
         }
         return summary;
+    }
+
+    public Tutor getTutor() {
+        return tutor;
     }
 }
